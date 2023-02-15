b394ca9d19f81b45c402fa055a696a2541dcd912
Marieli Ficagna <marieli.ficagna@gmail.com>
2023-02-15 17:33:03 -0300

    first commit

---
+++ src/operadorternario/OperadorTernario.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadorternario;
+
+/**
+ *
+ * @author Mari (48) 99618-5728
+ */
+public class OperadorTernario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1, n2, r;
+        n1 = 8;
+        n2 = 4;
+        r = n1>n2 ? n1:n2;
+        System.out.println("O maior número é "+r);
+        
+    }
+    
+}
